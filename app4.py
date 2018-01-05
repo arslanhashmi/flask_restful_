8101b41f746f3bd66364637323631e674e2c8d78
Taimoor Pasha <taimoorpasha2009@gmail.com>
2018-01-05 15:18:41 +0500

    Changes

--- app4.py
+++ app4.py
@@ -64,13 +64,7 @@ class Todo(Resource):
     """Get a todo task
     This will be added to the <strong>Implementation Notes</strong>.
     It lets you put very long text in your api.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.
+
     """
     abort_if_todo_doesnt_exist(todo_id)
 
